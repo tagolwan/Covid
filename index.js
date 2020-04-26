643804f9757f543bac8488362be4dcee0f70c733
tagolwan <takonwan43@gmail.com>
2020-04-27 00:23:34 +0700

    update home pages

--- index.js
+++ index.js
@@ -26,3 +26,25 @@ $(function () {
 
 
 })
+
+let Confirmed = document.getElementById('Confirmed');
+let Death = document.getElementById('Death');
+let Recovered = document.getElementById('Recovered');
+
+fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+		"x-rapidapi-key": "adfc52c44amsh606254c7c8f0cacp1fbe0ejsn0f99b718f00f"
+	}
+})
+.then(response => response.json().then(data => {
+    console.log(data);
+    Confirmed.innerHTML = data.total_cases;
+    Death.innerHTML = data.total_deaths;
+    Recovered.innerHTML = data.total_recovered;
+
+}))
+.catch(err => {
+	console.log(err);
+});
